f61e0c8fc6e4e5cb7a24c8bea345f7595536724b
Luiz Felipe <lffm1994@gmail.com>
2023-11-14 00:37:38 -0300

    Lidando com customizações

--- ecommerce/src/main/java/br/com/alura/ecommerce/EmailService.java
+++ ecommerce/src/main/java/br/com/alura/ecommerce/EmailService.java
@@ -2,13 +2,16 @@ package br.com.alura.ecommerce;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+import java.util.Map;
+
 public class EmailService {
 
     public static void main(String[] args) throws InterruptedException {
         var emailService = new EmailService();
         try (var service = new KafkaService(EmailService.class.getName(), "ECOMMERCE_SEND_EMAIL",
                 emailService::parse,
-                String.class)) {
+                String.class,
+                Map.of())) {
             service.run();
         }
     }
